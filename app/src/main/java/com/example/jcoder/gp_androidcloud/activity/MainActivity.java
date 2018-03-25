e1283d4216d5e0cd75daea2374cd71b584f94435
JCoder1995 <happyjcoder1995@gmail.com>
2018-03-25 18:55:42 +0800

    b

--- app/src/main/java/com/example/jcoder/gp_androidcloud/activity/MainActivity.java
+++ app/src/main/java/com/example/jcoder/gp_androidcloud/activity/MainActivity.java
@@ -1,19 +1,13 @@
 package com.example.jcoder.gp_androidcloud.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,16 +15,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
 import com.example.jcoder.gp_androidcloud.R;
-import com.example.jcoder.gp_androidcloud.enity.FileList;
-import com.example.jcoder.gp_androidcloud.fragment.HomeFragment;
-import com.example.jcoder.gp_androidcloud.utility.CustomHelper;
-import com.github.rubensousa.floatingtoolbar.FloatingToolbar;
-import com.guanaj.easyswipemenulibrary.EasySwipeMenuLayout;
 import com.jph.takephoto.app.TakePhoto;
 import com.jph.takephoto.app.TakePhotoImpl;
 import com.jph.takephoto.model.InvokeParam;
@@ -41,45 +29,40 @@ import com.jph.takephoto.permission.PermissionManager;
 import com.jph.takephoto.permission.TakePhotoInvocationHandler;
 import com.leon.lfilepickerlibrary.LFilePicker;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import me.yokeyword.fragmentation.BuildConfig;
-import me.yokeyword.fragmentation.Fragmentation;
 import me.yokeyword.fragmentation.SupportActivity;
 
-public class MainActivity extends SupportActivity
-        implements NavigationView.OnNavigationItemSelectedListener,TakePhoto.TakeResultListener,InvokeListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
      //获取当前Activity名称
      private static final String TAG = MainActivity.class.getName();
      //获取TakePhoto
      private TakePhoto takePhoto;
      private InvokeParam invokeParam;
      private Button uploadPhoto;
-     //文件显示相关
-     private RecyclerView recyclerView;
-     private List<String> listData;
-     private LayoutInflater inflater;
-     private MyAdapter myAdapter;
-     //定义底端Toolbar显示
-     private FloatingToolbar mFloatingToolbar;
+
+    // 再点一次退出程序时间设置
+    private static final long WAIT_TIME = 2000L;
+    private long TOUCH_TIME = 0;
+
+    private LinearLayout popLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //加载TakePhoto框架
-        getTakePhoto().onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
-        //获取拍照布局
-        // View contentView= LayoutInflater.from(this).inflate(R.layout.common_layout,null);
-        //初始化Menu
-
         setContentView(R.layout.activity_main);
         //设置Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //获取隐藏布局
+        popLayout =(LinearLayout)findViewById(R.id.popLayout);
+
+        popLayout.setVisibility(View.VISIBLE);
 
         //设置可滑动按钮 开启导航栏
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -87,64 +70,31 @@ public class MainActivity extends SupportActivity
         //加载导航栏布局
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
         //上传图片
         uploadPhoto= findViewById(R.id.btnPickBySelect);
-        initFileView();
-        initIData();
-        //设置底端Toolbar
-        mFloatingToolbar = findViewById(R.id.floatingToolbar);
-
-        mFloatingToolbar.setClickListener(new FloatingToolbar.ItemClickListener() {
-            @Override
-            public void onItemClick(MenuItem item) {
-                Toast.makeText(MainActivity.this,item.getItemId(),Toast.LENGTH_SHORT).show();
-
-
-            }
-
-            @Override
-            public void onItemLongClick(MenuItem item) {
-            Toast.makeText(MainActivity.this,"123123",Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
     }
 
     //获取TakePhoto实例
     @Override
     public void onSaveInstanceState(Bundle outPersistentState) {
-        getTakePhoto().onSaveInstanceState(outPersistentState);
         super.onSaveInstanceState(outPersistentState);
     }
 
-    //返回结果集
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        getTakePhoto().onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        PermissionManager.TPermissionType type=PermissionManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
-        PermissionManager.handlePermissionsResult(this,type,invokeParam,this);
-    }
-
-
     //监听返回键 如果导航栏在开启状态 自动回退导航栏
     @Override
-    public void onBackPressedSupport() {
+    public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
 
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            if (System.currentTimeMillis() - TOUCH_TIME < WAIT_TIME) {
+                finish();
+            } else {
+                TOUCH_TIME = System.currentTimeMillis();
+                Toast.makeText(this, R.string.press_again_exit, Toast.LENGTH_SHORT).show();
+            }
         }
-        super.onBackPressedSupport();
     }
 
     //加载Menu
@@ -209,100 +159,9 @@ public class MainActivity extends SupportActivity
         } else if (id == R.id.nav_send) {
 
         }
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    public TakePhoto getTakePhoto(){
-        if (takePhoto==null){
-            takePhoto= (TakePhoto) TakePhotoInvocationHandler.of(this).bind(new TakePhotoImpl(this,this));
-        }
-        return takePhoto;
-    }
-
-    @Override
-    public void takeSuccess(TResult result) {
-        Log.i(TAG,"takeSuccess：" + result.getImage().getCompressPath());
-    }
-
-    @Override
-    public void takeFail(TResult result, String msg) {
-        Log.i(TAG, "takeFail:" + msg);
-    }
-
-    @Override
-    public void takeCancel() {
-        Log.i(TAG, getResources().getString(R.string.msg_operation_canceled));
-    }
-
-    @Override
-    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
-        PermissionManager.TPermissionType type=PermissionManager.checkPermission(TContextWrap.of(this),invokeParam.getMethod());
-        if(PermissionManager.TPermissionType.WAIT.equals(type)){
-            this.invokeParam=invokeParam;
-        }
-        return type;
-    }
-
-    //加载文件显示
-    private void initFileView(){
-        recyclerView = findViewById(R.id.main_recycler);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        myAdapter = new MyAdapter(R.layout.file_list_show, null);
-
-        recyclerView.setAdapter(myAdapter);
-
-
-        inflater = getLayoutInflater();
-    }
-
-    //初始化数据
-    private void initIData() {
-        listData = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            listData.add("index is =" + i);
-        }
-        myAdapter.addData(listData);
-        myAdapter.notifyDataSetChanged();
-    }
-
-    //文件Adapter
-    public class MyAdapter extends BaseQuickAdapter<String, BaseViewHolder> {
-
-        MyAdapter(@LayoutRes int layoutResId, @Nullable List<String> data) {
-            super(layoutResId, data);
-        }
-
-        @Override
-        protected void convert(final BaseViewHolder helper, final String item) {
-
-            helper.getView(R.id.file_list_select).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Toast.makeText(MainActivity.this,"点击了CheckBox"+item,Toast.LENGTH_SHORT).show();
-                    mFloatingToolbar.show();
-
-                }
-            });
-
-            helper.getView(R.id.file_list_collect).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "收藏"+item, Toast.LENGTH_SHORT).show();
-                    EasySwipeMenuLayout easySwipeMenuLayout = helper.getView(R.id.es);
-
-                    easySwipeMenuLayout.resetStatus();
-                }
-            });
-            helper.getView(R.id.content).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "setOnClickListener", Toast.LENGTH_SHORT).show();
-
-                }
-            });
-        }
-    }
 }
