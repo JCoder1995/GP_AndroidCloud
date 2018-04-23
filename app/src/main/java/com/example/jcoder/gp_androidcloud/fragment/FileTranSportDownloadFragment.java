41708e3e740f6f9d1c42b0b3cf281d2678a5bc2e
JCoder1995 <happyjcoder1995@gmail.com>
2018-04-23 21:50:04 +0800

    文件上传问题 使用 BottomNavigationView +Fragment

---
+++ app/src/main/java/com/example/jcoder/gp_androidcloud/fragment/FileTranSportDownloadFragment.java
@@ -0,0 +1,30 @@
+package com.example.jcoder.gp_androidcloud.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jcoder.gp_androidcloud.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FileTranSportDownloadFragment extends Fragment {
+
+
+    public FileTranSportDownloadFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_file_tran_sport_download, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/jcoder/gp_androidcloud/fragment/FileTranSportOverFragment.java
@@ -0,0 +1,30 @@
+package com.example.jcoder.gp_androidcloud.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.jcoder.gp_androidcloud.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FileTranSportOverFragment extends Fragment {
+
+
+    public FileTranSportOverFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_file_tran_sport_over, container, false);
+    }
+
+}
